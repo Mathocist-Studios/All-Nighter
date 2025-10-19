06213e6d05f584e10a8d056c36e2faa921c08199
Aiden <57725407+ItzTheDodo@users.noreply.github.com>
2025-10-19 14:29:30 +0100

    Add GameMap and GameWorld classes; enhance map data handling and integrate texture atlas

--- core/src/main/java/com/mathochist/mazegame/World/GameMap.java
+++ core/src/main/java/com/mathochist/mazegame/World/GameMap.java
@@ -1,13 +1,11 @@
 package com.mathochist.mazegame.World;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.files.FileHandle;
-import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonReader;
 import com.badlogic.gdx.utils.JsonValue;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class GameMap {
 
@@ -20,13 +18,96 @@ public class GameMap {
         this.mapFile = mapFile;
         this.jsonReader = new JsonReader();
         this.mapData = jsonReader.parse(this.mapFile);
-        JsonValue matrixData = this.mapData.get("map_matrix");
 
+    }
+
+    public FileHandle getMapFile() {
+        return mapFile;
+    }
+
+    public JsonValue getMapData() {
+        return mapData;
+    }
+
+    public String getMapName() {
+        return mapData.get("metadata").getString("name");
+    }
+
+    public String getMapAuthor() {
+        return mapData.get("metadata").getString("author");
+    }
+
+    public String getMapVersion() {
+        return mapData.get("metadata").getString("version");
+    }
+
+    public int getTileWidth() {
+        return mapData.get("metadata").get("tilesets").getInt("tile_width");
+    }
+
+    public int getTileHeight() {
+        return mapData.get("metadata").get("tilesets").getInt("tile_height");
+    }
+
+    public int getMapWidth() {
+        return mapData.get("metadata").getInt("map_width");
+    }
+
+    public int getMapHeight() {
+        return mapData.get("metadata").getInt("map_height");
+    }
+
+    public int getSpawnX() {
+        return mapData.get("metadata").get("spawn_point").getInt("x");
+    }
+
+    public int getSpawnY() {
+        return mapData.get("metadata").get("spawn_point").getInt("y");
+    }
+
+    public Music getBackgroundMusic() {
+        String musicFile = mapData.get("metadata").getString("background_music");
+        return Gdx.audio.newMusic(Gdx.files.internal(musicFile));
+    }
+
+    public JsonValue getExitPoints() {
+        return mapData.get("metadata").get("exit_points");
+    }
+
+    public JsonValue getNPCs() {
+        return mapData.get("metadata").get("npcs");
+    }
+
+    public JsonValue getNPC(String npcName) {
+        JsonValue npcs = getNPCs();
+        for (JsonValue npc : npcs) {
+            if (npc.getString("name").equals(npcName)) {
+                return npc;
+            }
+        }
+        return null;
+    }
+
+    public String[] getTilesetRegionNames() {
+        JsonValue tileset = mapData.get("tiles");
         Json json = new Json();
+        return json.readValue(String[].class, tileset);
+    }
 
-        int[][] mapMatrix = json.readValue(int[][].class,  matrixData);
-        System.out.println(mapMatrix.length);
+    public int[] getCollisionTiles() {
+        JsonValue collisionTiles = mapData.get("collision_tiles");
+        Json json = new Json();
+        return json.readValue(int[].class, collisionTiles);
+    }
 
+    public int[][] getMapMatrix() {
+        JsonValue matrixData = this.mapData.get("map_matrix");
+        Json json = new Json();
+        return json.readValue(int[][].class,  matrixData);
+    }
+
+    public String getTextureAtlasFile() {
+        return mapData.get("metadata").getString("texture_atlas");
     }
 
 }
--- core/src/main/java/com/mathochist/mazegame/World/GameWorld.java
+++ core/src/main/java/com/mathochist/mazegame/World/GameWorld.java
@@ -1,4 +1,63 @@
 package com.mathochist.mazegame.World;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
 public class GameWorld {
+
+    private GameMap currentMap;
+    private TextureAtlas textureAtlas;
+
+    public GameWorld(FileHandle mapFile) {
+
+        this.currentMap = new GameMap(mapFile);
+
+        // Load texture atlas for the current map
+        this.textureAtlas = new TextureAtlas(Gdx.files.internal(this.currentMap.getTextureAtlasFile()));
+
+    }
+
+    public GameMap getCurrentMap() {
+        return currentMap;
+    }
+
+    public TextureAtlas getTextureAtlas() {
+        return textureAtlas;
+    }
+
+    public void render() {
+        // Rendering logic for the game world goes here
+    }
+
+    public int[] getCollisionLayer(int playerXPixels, int playerYPixels) {
+        // Logic to get collision layer data based on player position
+        return null;
+    }
+
+    public void dispose() {
+        textureAtlas.dispose();
+    }
+
+    public int pixelCoordsToTileIndex(int pixelCoord, int tileSize) {
+        return pixelCoord / tileSize;
+    }
+
+    public int[] getPlayerTilePosition(int playerXPixels, int playerYPixels) {
+        int tileX = pixelCoordsToTileIndex(playerXPixels, currentMap.getTileWidth());
+        int tileY = pixelCoordsToTileIndex(playerYPixels, currentMap.getTileHeight());
+        return new int[]{tileX, tileY};
+    }
+
+    public boolean isTileCollidable(int tileX, int tileY) {
+        // Logic to determine if a tile at (tileX, tileY) is collidable
+        return false;
+    }
+
+    public int[] getPixelBoundsOfTile(int tileX, int tileY) {
+        int pixelX = tileX * currentMap.getTileWidth();
+        int pixelY = tileY * currentMap.getTileHeight();
+        return new int[]{pixelX, pixelY, currentMap.getTileWidth(), currentMap.getTileHeight()};
+    }
+
 }
